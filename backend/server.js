5efef9759839f1762f150868b136e84ae3749c20
ratnesh <12211080cs@gmail.com>
2026-02-05 22:30:49 +0530

    finally

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000', 
   'http://localhost:5001',
-  'https:https://notes-ten-rouge.vercel.app' 
+  'https://notes-git-main-ratneshs-projects-12b69610.vercel.app/notes' 
 ];
 
 app.use(cors({
